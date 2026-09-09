f0d158f7350db08ffcb406847aad7f4d40961f9f
Abdullahazeem2 <abdullahazeemkhi@gmail.com>
2026-09-09 13:16:47 +0500

    import bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import CoffeeMenu from "./pages/coffeeMenu";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import About from "./pages/About";
